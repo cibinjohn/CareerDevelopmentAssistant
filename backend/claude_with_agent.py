0673a0ac1f10c1cb27d8a7795ec1b08f2964b814
johnj <cibinjohnjoseph@gmail.com>
2026-05-04 19:02:50 -0400

    First custom tool and agent

---
+++ backend/claude_with_agent.py
@@ -0,0 +1,73 @@
+from langchain_anthropic import ChatAnthropic
+from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
+
+from backend.tools import add_tool
+from config.constants import ANTHROPIC_BASE_MODEL
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
+
+class ClaudeLangchainAgent:
+    def __init__(self):
+        self.tools = [add_tool]
+
+        self.llm = ChatAnthropic(
+            model=ANTHROPIC_BASE_MODEL,  # fast & cheap (good for dev)
+            temperature=0.7,
+            streaming=True
+        )
+
+        self.parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+
+    def get_basic_streaming_response(self, message:str):
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                (
+                    "system",
+                    """
+                    You are a research assistant that will help generate a research paper.
+                    Answer the user query and use neccessary tools. 
+                    IMPORTANT: Always use the tool result as the answer if you use 'add' tool
+                    Wrap the output in this format and provide no other text\n{format_instructions}
+                    """,
+                ),
+                ("placeholder", "{chat_history}"),
+                ("human", "{query}"),
+                ("placeholder", "{agent_scratchpad}"),
+            ]
+        ).partial(format_instructions=self.parser.get_format_instructions())
+
+        agent = create_tool_calling_agent(
+            llm=self.llm,
+            prompt=prompt,
+            tools=self.tools
+        )
+
+        agent_executor = AgentExecutor(agent=agent, tools=self.tools, verbose=True)
+
+        raw_response = agent_executor.invoke({"query": message})
+        try:
+            structured_response = self.parser.parse(raw_response.get("output")[0]["text"])
+            print("structured_response :",structured_response)
+            return structured_response
+        except Exception as e:
+            print("Error parsing response", e, "Raw Response - ", raw_response)
+
+if __name__ == "__main__":
+    agent = ClaudeLangchainAgent()
+    agent.get_basic_streaming_response("What is 3+3?")
+
+
